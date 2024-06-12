7f7ff343f9c10f33c31f48bc52ae28c303d3dcd9
Joseph <JPMeehan5@Gmail.com>
2024-06-12 09:29:57 -0700

    [#58] Delete all button (#59)
    * Delete all button
    
    * Update src/module/canvas/CardLayer.mjs
    
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>
    
    * Removed unneeded i18n string
    
    * Added message to ui.notifications.warn call
    
    * Adjusted fail state to return null
    
    ---------
    
    Co-authored-by: Zhell <50169243+krbz999@users.noreply.github.com>

--- src/module/canvas/CardLayer.mjs
+++ src/module/canvas/CardLayer.mjs
@@ -154,6 +154,36 @@ export default class CardLayer extends PlaceablesLayer {
   };
 
   /** @override */
+  async deleteAll() {
+    const type = this.constructor.documentName;
+    if (!game.user.isGM) {
+      throw new Error(`You do not have permission to delete ${type} objects from the Scene.`);
+    }
+    const proceed = await foundry.applications.api.DialogV2.confirm({
+      window: {
+        title: "CONTROLS.ClearAll",
+        icon: "fa-solid fa-cards"
+      },
+      content: game.i18n.format("CONTROLS.ClearAllHint", {type}),
+      rejectClose: false,
+      modal: true
+    });
+    if (proceed) {
+      const cardCollection = canvas.scene.getFlag(MODULE_ID, "cardCollection");
+      if (!cardCollection) {
+        ui.notifications.warn("CARDS.NoCards", {localize: true});
+        return null;
+      }
+      for (const uuid of cardCollection) {
+        const card = fromUuidSync(uuid);
+        await card.unsetFlag(MODULE_ID, canvas.scene.id);
+      }
+      ui.notifications.info(game.i18n.format("CONTROLS.DeletedObjects", {count: cardCollection.length, type}));
+      return canvas.scene.unsetFlag(MODULE_ID, "cardCollection");
+    }
+  }
+
+  /** @override */
   async _onDeleteKey(event) {
     if (game.paused && !game.user.isGM) {
       ui.notifications.warn("GAME.PausedWarning", {localize: true});
