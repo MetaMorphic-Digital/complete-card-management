02a52183a4fc729dd2e932c0adaaacff2d12e48c
Joseph <JPMeehan5@Gmail.com>
2024-06-17 13:44:08 -0700

    Improved error message

--- src/module/canvas/CardLayer.mjs
+++ src/module/canvas/CardLayer.mjs
@@ -123,11 +123,15 @@ export default class CardLayer extends PlaceablesLayer {
 
     this.preview = this.addChild(new PIXI.Container());
 
+    /** @type {Array<Card | Cards>} */
     const documents = this.getDocuments();
     const promises = documents.map((doc) => {
       // Preemptively filtering out drawings that would fail
       const data = doc.getFlag(MODULE_ID, canvas.scene.id);
-      if (!data) return;
+      if (!data) {
+        console.warn("No canvas data found for", doc.name);
+        return;
+      }
       const syntheticDoc = new CanvasCard(doc);
       doc.canvasCard = syntheticDoc;
       const obj = (syntheticDoc._object = this.createObject(syntheticDoc));
