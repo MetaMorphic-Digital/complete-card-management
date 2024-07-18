b1fd9e81be054b88cd6f2f3e83dfe49ac8118070
Zhell <kennethzhell@gmail.com>
2024-07-18 14:13:19 +0200

    [#32] Allow for recalling specific card directly from Deck sheet.

---
+++ src/module/api/recall-card.mjs
@@ -0,0 +1,34 @@
+/**
+ * Recall a drawn card from a deck.
+ * @param {Cards} deck          The deck to recall a drawn card to.
+ * @param {string} cardId       The id of the card to recall.
+ * @returns {Promise<Card>}     A reference to the recalled card belonging to its original parent.
+ */
+export default async function recallCard(deck, cardId) {
+  if (deck.type !== "deck") return;
+  const card = deck.cards.get(cardId);
+  if (!card || !card.drawn) return;
+  const clone = findClone(card);
+  ChatMessage.implementation.create({
+    content: game.i18n.format("CCM.CardSheet.RecalledCard", {
+      card: card.link,
+      deck: deck.link
+    })
+  });
+  return clone ? clone.recall() : card.recall();
+}
+
+/**
+ * Find the "clone" of a card in a hand or pile.
+ * @param {Card} card       The card of which to find a clone.
+ * @returns {Card|void}     The clone if any is found.
+ */
+function findClone(card) {
+  for (const cards of game.cards) {
+    if (cards.type === "deck") continue;
+    const c = cards.cards.find(c => {
+      return (c.source === card.parent) && (c.id === card.id);
+    });
+    if (c) return c;
+  }
+}
