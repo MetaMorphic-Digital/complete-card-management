8d1c714b430617b029535f42ee4cbee54e541901
Joseph <JPMeehan5@Gmail.com>
2024-08-06 14:10:47 -0700

    Implemented ccm.api.triangle

--- src/module/api/layout.mjs
+++ src/module/api/layout.mjs
@@ -1,5 +1,4 @@
 import {MODULE_ID} from "../helpers.mjs";
-import {placeCard} from "./singles.mjs";
 
 /**
  * Creates a grid of placed cards
@@ -70,6 +69,115 @@ export async function grid(config, options = {}) {
             [canvas.scene.id]: {
               x: offsetX + j * (cardWidth + spacing.x),
               y: offsetY + i * (cardHeight + spacing.y),
+              rotation: card.rotation,
+              sort: card.sort
+            }
+          }
+        }
+      };
+      updateData.push(cardUpdate);
+    }
+  }
+
+  await config.to.updateEmbeddedDocuments("Card", updateData);
+  const currentCards = new Set(canvas.scene.getFlag(MODULE_ID, "cardCollection")).union(
+    new Set(cards.map((card) => card.uuid))
+  );
+  await canvas.scene.setFlag(MODULE_ID, "cardCollection", Array.from(currentCards));
+  return cards;
+}
+
+/**
+ * Creates a pyramid of placed cards
+ * @param {object} config                       Mandatory configuration object
+ * @param {Cards} config.from                   The Cards document to draw from
+ * @param {Cards} config.to                     The Cards document to put the cards into
+ * @param {number} config.base                  Number of cards per side
+ * @param {object} [options]                    Options modifying the layout
+ * @param {number} [options.how=0]              How to draw, a value from CONST.CARD_DRAW_MODES
+ * @param {object} [options.updateData={}]      Modifications to make to each Card as part of
+ *                                              the draw operation, for example the displayed face
+ * @param {number} [options.horizontalSpacing]  Spacing between cards horizontally
+ *                                              Defaults to `canvas.grid.sizeX`
+ * @param {number} [options.verticalSpacing]    Spacing between cards vertically
+ *                                              Defaults to `canvas.grid.sizeY`
+ * @param {number} [options.defaultWidth=2]     Default width of a card in grid squares
+ * @param {number} [options.defaultHeight=3]    Default height of a card in grid squares
+ * @param {number} [options.offsetX]            Adjust X offset from the top left of the scene
+ * @param {number} [options.offsetY]            Adjust Y offset from the top left of the scene
+ * @param {"UP" | "DOWN" | "LEFT" | "RIGHT"} [options.direction] Direction to orient the pyramid
+ * @returns {Promise<Card[]>}                   A promise that resolves to the drawn cards.
+ */
+export async function triangle(config, options = {}) {
+  if (!canvas.scene) {
+    throw new Error("Not viewing a canvas to place cards");
+  }
+  if (config.from.type !== "deck") {
+    throw new Error("You can only create a grid from a deck");
+  }
+  if (!canvas.scene.canUserModify(game.user, "update")) {
+    throw new Error("Placing a card requires updating the scene");
+  }
+
+  const {sceneHeight, sceneWidth, sceneX, sceneY} = canvas.dimensions;
+  const cardWidth = canvas.grid.sizeX * (options.defaultWidth ?? 2);
+  const cardHeight = canvas.grid.sizeY * (options.defaultHeight ?? 3);
+  const spacing = {
+    x: options.horizontalSpacing ?? canvas.grid.sizeX,
+    y: options.verticalSpacing ?? canvas.grid.sizeY
+  };
+  const direction = options.direction ?? "UP";
+  const direction_x = direction === "LEFT" ? -1 : 1;
+  const direction_y = direction === "UP" ? -1 : 1;
+  const isVertical = ["UP", "DOWN"].includes(direction);
+
+  // Only need spacing between cards, not either end, so 1 less than # cards
+  const totalHeight = config.base * (spacing.y + cardHeight) - spacing.y;
+  const totalWidth = config.base * (spacing.x + cardWidth) - spacing.x;
+
+  if ((totalWidth > sceneWidth) || (totalHeight > sceneHeight)) {
+    throw new Error("Not enough room on canvas to place cards");
+  }
+
+  const drawCount = (config.base * (config.base + 1)) / 2;
+  const cards = await config.to.draw(config.from, drawCount, {
+    how: options.how,
+    updateData: options.updateData ?? {}
+  });
+
+  const updateData = [];
+
+  for (let i = 0; i < config.base; i++) {
+    let offsetX = sceneX + (options.offsetX ?? 0);
+    let offsetY = sceneY + (options.offsetY ?? 0);
+    switch (direction) {
+      case "DOWN":
+        offsetX += (spacing.x + cardWidth) * 0.5 * i;
+        break;
+      case "UP":
+        offsetX += (spacing.x + cardWidth) * 0.5 * i;
+        offsetY += totalHeight - cardHeight;
+        break;
+      case "RIGHT":
+        offsetY += (spacing.y + cardHeight) * 0.5 * i;
+        break;
+      case "LEFT":
+        offsetY += (spacing.y + cardHeight) * 0.5 * i;
+        offsetX += totalWidth - cardWidth;
+        break;
+    }
+    for (let j = 0; j < (config.base - i); j++) {
+      const index = i * config.base + j - ((i * (i - 1)) / 2);
+      const loop_x = isVertical ? j : i;
+      const loop_y = isVertical ? i : j;
+      const card = cards[index];
+      const cardUpdate = {
+        _id: card._id,
+        flags: {
+          [MODULE_ID]: {
+            [canvas.scene.id]: {
+              x: offsetX + loop_x * (cardWidth + spacing.x) * direction_x,
+              y: offsetY + loop_y * (cardHeight + spacing.y) * direction_y,
               rotation: card.rotation,
               sort: card.sort
             }
