a68df269ad5db8b5cc8afdd3fd68b29d42778763
Joseph <JPMeehan5@Gmail.com>
2024-09-11 14:32:52 -0700

    Bug fixes

--- src/module/canvas/CanvasCard.mjs
+++ src/module/canvas/CanvasCard.mjs
@@ -265,7 +265,7 @@ export default class CanvasCard extends foundry.abstract.DataModel {
       } else if ((!originInside || newCard) && destinationInside) {
         region._triggerEvent(CONFIG.CCM.REGION_EVENTS.CARD_MOVE_IN, eventData);
         // Crude way to approximate if this is going to trigger a pass event.
-        makingMove ||= region.behaviors.some(b => b.type === "moveCard");
+        makingMove ||= region.behaviors.some(b => b.type === "complete-card-management.moveCard");
       }
     }
     // Don't check deck drops if there's a region setup, and only original user does this part
