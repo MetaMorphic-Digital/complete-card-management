8b47bcf2fc385e951ee0ddf55d630ef138d370e0
Joseph <JPMeehan5@Gmail.com>
2026-02-19 11:37:59 -0800

    Also ensure card meshes z index is correct

--- src/module/canvas/CardLayer.mjs
+++ src/module/canvas/CardLayer.mjs
@@ -118,7 +118,7 @@ export default class CardLayer extends foundry.canvas.layers.PlaceablesLayer {
     itf.cardMeshes.sortableChildren = true;
     itf.cardMeshes.eventMode = "none";
     itf.cardMeshes.interactiveChildren = false;
-    itf.cardMeshes.zIndex = 100;
+    itf.cardMeshes.zIndex = this.getZIndex();
 
     // Layer functionality
     // Inherited from InteractionLayer
